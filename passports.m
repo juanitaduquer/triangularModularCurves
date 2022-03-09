54c138e411e40f7843e6487ea98177d076f2dd91
Juanita Duque Rosero <juanitaduquer@gmail.com>
2022-03-08 22:40:10 -0500

    Added passports outputs

---
+++ passports.m
@@ -0,0 +1,54 @@
+AttachSpec("~/Documents/Code/Belyi/Code/spec");
+load "listsLowGenus.m";
+
+output := "PassportsOutput.txt";
+
+genusPassport := function(t,genus)
+  a,b,c,p,q,l := Explode(t);
+  if l eq 1 then
+    G := PSL(2,q);
+  else
+    G := PGL(2,q);
+  end if;
+  pass := PassportRepresentatives(G : abc := [a,b,c]);
+  if #pass ne 1 then
+    print "error",t;
+  end if;
+  partition := pass[1][1];
+  ramification := 0;
+  for i in partition do
+    for j in i do
+      ramification := ramification + (j[1]-1)*(j[2]);
+    end for;
+  end for;
+  if genus ne ((1/2)*(-2*(q+1)+ramification+2)) then
+    print "error",t;
+  end if;
+  Write(output, Sprint(pass[1][2], "Magma"));
+  Write(output, ";");
+  Write(output, "\n");
+  return true;
+end function;
+
+Write(output, "******************************************");
+Write(output, "Genus 0");
+Write(output, "******************************************");
+Write(output, "\n");
+for t in genus0 do
+  Write(output, Sprint(t));
+  Write(output, ";");
+  Write(output, "\n");
+  genusPassport(t,0);
+  Write(output, "\n");
+end for;
+Write(output, "******************************************");
+Write(output, "Genus 1");
+Write(output, "******************************************");
+Write(output, "\n");
+for t in genus1 do
+  Write(output, Sprint(t));
+  Write(output, ";");
+  Write(output, "\n");
+  genusPassport(t,1);
+  Write(output, "\n");
+end for;
